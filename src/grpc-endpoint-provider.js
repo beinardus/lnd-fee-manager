8073828ef1c95032a97df94233feec93b42e78bc
koen van beinum <beinum@hotmail.com>
2023-09-01 01:02:58 +0200

    undo cert update

--- src/grpc-endpoint-provider.js
+++ src/grpc-endpoint-provider.js
@@ -19,25 +19,26 @@ const packageDefinition = protoLoader.loadSync(
   ["./lnd/lightning.proto", "./lnd/router.proto"],
   loaderOptions
 );
-const lnrpc = grpc.loadPackageDefinition(packageDefinition).lnrpc;
-const routerrpc = grpc.loadPackageDefinition(packageDefinition).routerrpc;
+const { lnrpc, routerrpc } = grpc.loadPackageDefinition(packageDefinition);
 
 process.env.GRPC_SSL_CIPHER_SUITES = "HIGH+ECDSA";
 const tlsCert = fs.readFileSync(TLS_CERT_PATH);
 const sslCreds = grpc.credentials.createSsl(tlsCert);
 const macaroon = fs.readFileSync(MACAROON_PATH).toString("hex");
-const macaroonCreds = grpc.credentials.createFromMetadataGenerator(function (
-  args,
-  callback
-) {
-  let metadata = new grpc.Metadata();
-  metadata.add("macaroon", macaroon);
-  callback(null, metadata);
-});
+
+const macaroonCreds = grpc.credentials.createFromMetadataGenerator(
+  (_args, callback) => {
+    let metadata = new grpc.Metadata();
+    metadata.add("macaroon", macaroon);
+    callback(null, metadata);
+  }
+);
+
 const creds = grpc.credentials.combineChannelCredentials(
   sslCreds,
   macaroonCreds
 );
+
 const client_lightning = new lnrpc.Lightning(GRPC_LOCATION, creds);
 const client_router = new routerrpc.Router(GRPC_LOCATION, creds);
 
