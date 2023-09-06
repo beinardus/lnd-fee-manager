a2943d34f7c20708db207705fc564507ae35b35d
koen van beinum <beinum@hotmail.com>
2023-09-06 16:48:33 +0200

    Umbrel installation

--- src/settings-provider.js
+++ src/settings-provider.js
@@ -1,7 +1,7 @@
 const GRPC_LOCATION = process.env.GRPC_LOCATION || "dutchbtc.ddns.net:10009";
 const MACAROON_PATH = process.env.MACAROON_PATH || "./lnd/admin.macaroon";
 const TLS_CERT_PATH = process.env.TLS_CERT_PATH || "./lnd/tls.cert";
-const DATABASE_PATH = process.env.DATABASE_PATH || "./database.db";
+const DATABASE_PATH = process.env.DATABASE_PATH || "./data/database.db";
 const LOG_LEVEL = process.env.LOG_LEVEL || "info";
 
 export {
