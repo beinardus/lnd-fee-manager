891e7a4400bba081f62110b54e9e211f915918dd
koen van beinum <beinum@hotmail.com>
2023-09-08 01:46:52 +0200

    refactoring

--- src/update-fees.js
+++ src/update-fees.js
@@ -1,26 +1,25 @@
-import { exec } from 'child_process';
+import { exec } from "child_process";
 import logger from "./winston-plugin.js";
 
-const UPDATE_CMD = process.env.UPDATE_CMD || 'run-charge';
+const UPDATE_CMD = process.env.UPDATE_CMD || "run-charge";
 
 const execPromise = (command) => {
-    return new Promise((resolve, reject) => {
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                reject(error)
-            }
-            resolve(stderr || stdout);
-        });
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(stderr || stdout);
     });
+  });
 };
 
-const exportFees = async () => {
-    try {
-        logger.info(await execPromise(UPDATE_CMD));
-    }
-    catch (e) {
-        logger.error(`exportFee cmd: <${UPDATE_CMD}>`, e);
-    }
+const updateFees = async () => {
+  try {
+    logger.info(await execPromise(UPDATE_CMD));
+  } catch (e) {
+    logger.error(`updateFees cmd: <${UPDATE_CMD}>`, e);
+  }
 };
 
-export default exportFees;
+export default updateFees;
